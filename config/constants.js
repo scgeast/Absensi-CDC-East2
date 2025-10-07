8dfc7d4e748f99c70219aaf81bf43cbe6c6d8df4
scgeast <scgeast@gmail.com>
2025-10-07 21:49:09 +0700

    Update constants.js

--- config/constants.js
+++ config/constants.js
@@ -1,1 +1,47 @@
+// Application Constants
+export const COLLECTIONS = {
+    ABSENSI: 'absensi',
+    ABSEN_TABEL: 'absenTabel',
+    OVERTIME: 'overtime',
+    OVERTIME_RECORDS: 'overtimeRecords',
+    CUTI: 'cuti',
+    USERS: 'users',
+    SETTINGS: 'settings',
+    OUTPUT: 'output'
+};
 
+export const ROLES = {
+    ADMIN: 'admin',
+    USER: 'user'
+};
+
+export const SHIFT_CODES = {
+    MORNING: '1',
+    AFTERNOON: '2',
+    NIGHT: '3',
+    MORNING_PART: '11',
+    AFTERNOON_PART: '22',
+    LEAVE: 'C',
+    OFF: 'O'
+};
+
+export const SHIFT_COLORS = {
+    '1': 'shift-1',
+    '2': 'shift-2',
+    '3': 'shift-3',
+    '11': 'shift-11',
+    '22': 'shift-22',
+    'C': 'shift-C',
+    'O': 'shift-O'
+};
+
+export const DEFAULT_SETTINGS = {
+    MAX_DATE_RANGE: 31,
+    DEFAULT_LEAVE_DAYS: 12,
+    PASSWORD_MIN_LENGTH: 6
+};
+
+export const PLANT_OPTIONS = [
+    "Denpasar2", "Gianyar", "Manyar", "Manukan", 
+    "Gempol", "Kediri", "Krian"
+];
