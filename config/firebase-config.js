1ca220e518830461bc1d442452352e3775083c15
scgeast <scgeast@gmail.com>
2025-10-08 09:42:17 +0700

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,1 +1,9 @@
-
+// Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCs_65P_SLx529UwLcQO8cF69yD1yLOgKY",
+    authDomain: "blueiiifirebase.firebaseapp.com",
+    projectId: "blueiiifirebase",
+    storageBucket: "blueiiifirebase.firebasestorage.app",
+    messagingSenderId: "194236727932",
+    appId: "1:194236727932:web:6b10d6302d5c56ebac3ff9"
+};
